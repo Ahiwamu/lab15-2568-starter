65be82b5c9d87bc41b7701c01f8e54c91084472c
Ahiwamu <niceziio08@gmail.com>
2025-09-26 16:50:54 +0700

    finished

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,13 @@ const app: any = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
+app.get("/", (req: Request, res: Response) => {
+  res.json({
+    success: true,
+    message: "lab 15 API service successfully"
+  });
+});
+
 app.get("/me", (req: Request, res: Response) => {
   res.status(200).json({
     success: true,
